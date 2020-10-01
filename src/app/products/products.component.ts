4940d69624b3611f8f896f33b6d048723500829f
Avi-Cohen <aviko.cohen@gmail.com>
2020-10-01 11:28:50 +0300

    added and implemented product svc

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,5 +1,6 @@
+import { ProductService } from "./product.service";
 import { Component, OnInit } from "@angular/core";
-import { IProduct } from './product'
+import { IProduct } from "./product";
 
 @Component({
   selector: "pm-products",
@@ -11,89 +12,47 @@ export class ProductsComponent implements OnInit {
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
-  
-  private _listFiler: string;
-  public get listFiler(): string {
-    return this._listFiler;
+  errorMessage: string;
+
+  private _listFilter: string;
+  public get listFilter(): string {
+    return this._listFilter;
   }
-  public set listFiler(value: string) {
-    this._listFiler = value;
-    this.filteredProducts = this.listFiler ? this.performFilter(this.listFiler) : this.products;
+  public set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredProducts = this.listFilter
+      ? this.performFilter(this.listFilter)
+      : this.products;
   }
 
-  filteredProducts:IProduct[];
-  products: IProduct[] = [
-    {
-      productId: 1,
-      productName: "Leaf Rake",
-      productCode: "GDN-0011",
-      releaseDate: "March 19, 2019",
-      description: "Leaf rake with 48-inch wooden handle.",
-      price: 19.95,
-      starRating: 3.2,
-      imageUrl: "assets/images/leaf_rake.png",
-    },
-    {
-      productId: 2,
-      productName: "Garden Cart",
-      productCode: "GDN-0023",
-      releaseDate: "March 18, 2019",
-      description: "15 gallon capacity rolling garden cart",
-      price: 32.99,
-      starRating: 4.2,
-      imageUrl: "assets/images/garden_cart.png",
-    },
-    {
-      productId: 5,
-      productName: "Hammer",
-      productCode: "TBX-0048",
-      releaseDate: "May 21, 2019",
-      description: "Curved claw steel hammer",
-      price: 8.9,
-      starRating: 4.8,
-      imageUrl: "assets/images/hammer.png",
-    },
-    {
-      productId: 8,
-      productName: "Saw",
-      productCode: "TBX-0022",
-      releaseDate: "May 15, 2019",
-      description: "15-inch steel blade hand saw",
-      price: 11.55,
-      starRating: 3.7,
-      imageUrl: "assets/images/saw.png",
-    },
-    {
-      productId: 10,
-      productName: "Video Game Controller",
-      productCode: "GMG-0042",
-      releaseDate: "October 15, 2018",
-      description: "Standard two-button video game controller",
-      price: 35.95,
-      starRating: 4.6,
-      imageUrl: "assets/images/xbox-controller.png",
-    },
-  ];
+  filteredProducts: IProduct[];
+  products: IProduct[] = [];
 
-
-  constructor() {
-    this.filteredProducts = this.products;
-    this.listFiler = 'cart'
-  }
+  constructor(private productService: ProductService) {}
 
   performFilter(filterBy: string): IProduct[] {
     filterBy = filterBy.toLocaleLowerCase();
-    return this.products.filter((product: IProduct) =>
-      product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+    return this.products.filter(
+      (product: IProduct) =>
+        product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1
+    );
   }
 
   toggleImage(): void {
     this.showImage = !this.showImage;
   }
 
-  ngOnInit(): void  {}
-
   onRatingClicked(message: string): void {
-    this.pageTitle = 'Product List: ' + message;
+    this.pageTitle = "Product List: " + message;
+  }
+
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe({
+      next: products => {
+        this.products = products;
+        this.filteredProducts = this.products;
+      },
+      error: err => this.errorMessage = err
+    });
   }
 }
